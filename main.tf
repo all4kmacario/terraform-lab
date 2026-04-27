530b94028bcf4ca43ba5098e2a43d2dae368faf9
all4kmacario <kmacario@all4inc.com>
2026-04-27 12:00:00 -0700

    Add Docker Terraform test

--- main.tf
+++ main.tf
@@ -1,7 +1,30 @@
 terraform {
   required_version = ">=1.14.0"
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.6"
+    }
+  }
 }
 
-output "hello" {
-  value = "Hello from Terraform on WSL"
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  name  = "terraform-ngix"
+  image = docker_image.nginx.image_id
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
+output "nginx_url" {
+  value = "http://localhost:8080"
 }
